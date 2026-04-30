5bbd9c542a6553163249c29389b53564d5f718fc
Miguel Duarte <miguelcassiob@gmail.com>
2026-04-29 21:30:43 -0300

    Realizando alteração no Simulador para utilização do DTO

--- AlertaSimulador/Program.cs
+++ AlertaSimulador/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Shared;
 using System.Net.Http.Json;
+using Shared.DTOs;
 
 
 var http = new HttpClient();
@@ -11,7 +12,7 @@ Console.WriteLine("--- SIMULADOR DE SENSOR INDUSTRIAL INICIADO ---");
 while (true)
 {
     // Criando o objeto conforme sua classe no Shared
-    var leitura = new LeituraSensor
+    var leituraDto = new LeituraSensorDTO
     {
         SensorNome = "Sensor_Caldeira_A1",
         Valor = new Random().Next(20, 110), // Simula de 20 a 110 graus
@@ -20,7 +21,7 @@ while (true)
     };
 
     // Enviando para a API (Endpoint de recepção)
-    var response = await http.PostAsJsonAsync(urlApi, leitura);
+    var response = await http.PostAsJsonAsync(urlApi, leituraDto);
 
     if (!response.IsSuccessStatusCode)
     {
@@ -31,10 +32,10 @@ while (true)
     }
     else
     {
-        Console.Write($"[{leitura.DataHora:HH:mm:ss}] Valor enviado: {leitura.Valor}°C ");
+        Console.Write($"[{leituraDto.DataHora:HH:mm:ss}] Valor enviado: {leituraDto.Valor}°C ");
 
         // Destaque visual caso gere um alerta (conforme sua regra de > 90 na Service)
-        if (leitura.Valor > 90)
+        if (leituraDto.Valor > 90)
         {
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine(" -> [ALERTA GERADO]");
