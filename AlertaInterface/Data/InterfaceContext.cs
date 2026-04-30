6c11cc320acbca0471204a29d39e5500cd47cc4b
Miguel Duarte <miguelcassiob@gmail.com>
2026-04-30 20:19:31 -0300

    Modificação na interface para persistencia dos dados

---
+++ AlertaInterface/Data/InterfaceContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlertaInterface.Data
+{
+    class InterfaceContext: DbContext
+    {
+        public DbSet<AlertaDTO> AlertasCache { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=cache_interface.db");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AlertaDTO>().HasKey(a => a.Id);
+        }
+    }
+}
--- AlertaInterface/ViewModels/MainViewModel.cs
+++ AlertaInterface/ViewModels/MainViewModel.cs
@@ -5,23 +5,22 @@ using System.Text.Json;
 using System.Windows;
 using System.Windows.Input;
 using AlertaInterface.Commands;
+using AlertaInterface.Data; 
 using Shared.DTOs;
+using Microsoft.EntityFrameworkCore; 
 
 namespace AlertaInterface.ViewModels
 {
     public class MainViewModel : BaseViewModel
     {
-        
         public ObservableCollection<AlertaDTO> ListaAlertas { get; set; }
-
         public ICommand CarregarAlertasCommand { get; }
 
         public MainViewModel()
         {
-            
             ListaAlertas = new ObservableCollection<AlertaDTO>();
 
-
+            
             CarregarAlertasCommand = new RelayCommand(CarregarAlertas);
 
             CarregarAlertas();
@@ -34,26 +33,61 @@ namespace AlertaInterface.ViewModels
                 using var http = new HttpClient();
                 var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
 
-              
+             
                 var dados = await http.GetFromJsonAsync<List<AlertaDTO>>(
                     "https://localhost:7127/api/Alerta", options);
 
                 if (dados != null)
                 {
-                    Application.Current.Dispatcher.Invoke(() =>
+
+                    using (var db = new InterfaceContext())
                     {
-                        ListaAlertas.Clear();
-                        foreach (var item in dados)
-                        {
-                            ListaAlertas.Add(item);
-                        }
-                    });
+                        db.Database.EnsureCreated();
+
+                        var cacheAntigo = db.AlertasCache.ToList();
+                        db.AlertasCache.RemoveRange(cacheAntigo);
+
+                        db.AlertasCache.AddRange(dados);
+                        await db.SaveChangesAsync();
+                    }
+
+                    AtualizarInterface(dados);
                 }
             }
             catch (Exception ex)
             {
-                MessageBox.Show($"Certifique-se de que a API está rodando na porta 7127.\nErro: {ex.Message}");
+                
+                try
+                {
+                    using var db = new InterfaceContext();
+                    if (db.Database.CanConnect())
+                    {
+                        var cache = db.AlertasCache.ToList();
+                        if (cache.Any())
+                        {
+                            AtualizarInterface(cache);
+                            MessageBox.Show("Aviso: API Offline. Exibindo dados do cache local (Persistência WPF).");
+                        }
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show($"Erro de conexão e cache vazio: {ex.Message}");
+                }
             }
+        }
+
+      
+        private void AtualizarInterface(List<AlertaDTO> lista)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                ListaAlertas.Clear();
+                foreach (var item in lista)
+                {
+                    ListaAlertas.Add(item);
+                }
+            });
         }
     }
 }
