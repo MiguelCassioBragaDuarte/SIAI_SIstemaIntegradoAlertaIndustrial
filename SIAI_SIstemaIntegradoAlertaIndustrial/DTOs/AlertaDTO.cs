c320ee805d437cf3ea5ac885911e7496c6243d27
Miguel Duarte <miguelcassiob@gmail.com>
2026-04-29 21:27:45 -0300

    Criação da Pasta e arquivos DTO dentro do Shared

---
+++ SIAI_SIstemaIntegradoAlertaIndustrial/DTOs/AlertaDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DTOs
+{
+    public class AlertaDTO
+    {
+        public int Id { get; set; }
+        public string Mensagem { get; set; }
+        public int NivelGravidade { get; set; }
+        public DateTime DataHoraLeitura { get; set; } // Dado que vem da Leitura
+    }
+}
---
+++ SIAI_SIstemaIntegradoAlertaIndustrial/DTOs/LeituraSensorDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DTOs
+{
+    public class LeituraSensorDTO
+    {
+        public string SensorNome { get; set; }
+        public double Valor { get; set; }
+        public string UnidadeMedida { get; set; }
+        public DateTime DataHora { get; set; }
+    }
+}
